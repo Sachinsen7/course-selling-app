75ecb99705a5403a13f3def40c9122d1365fac9d
Sachin Sen <sachinsen1920@gmail.com>
2025-07-01 22:17:13 +0530

    refactor: restructure database connection and server start logic

--- Course Selling App/index.js
+++ Course Selling App/index.js
@@ -5,10 +5,21 @@ const { userRouter } = require("./routes/user")
 const { courseRouter } = require("./routes/course")
 const { adminRouter } = require("./routes/admin")
 app.use(express.json())
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 
+dotenv.config()
 
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/admin", adminRouter)
 app.use("/api/v2/course", courseRouter)
 
-app.listen(3000)
+async function connectDB(){
+    await mongoose.connect(process.env.MONGO_URI)
+    app.listen(3000)
+    console.log("Listening on port 3000")
+}
+
+connectDB()
+
+
