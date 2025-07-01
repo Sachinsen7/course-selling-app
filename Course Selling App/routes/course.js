2d1381f801ec660c95d4ffce7ca3152a4615e64b
Sachin Sen <sachinsen1920@gmail.com>
2025-07-01 22:23:44 +0530

    feat: add course routes for purchase and preview endpoints

---
+++ Course Selling App/routes/course.js
@@ -0,0 +1,17 @@
+const { Router } = require("express")
+const { CourseModel} = require("../db/db")
+
+const courseRouter = Router()
+
+courseRouter.post("/purchase", (req, res) => {
+
+})
+
+
+courseRouter.get("/preview", (req, res) => {
+
+})
+
+module.exports = {
+    courseRouter
+}
