f299dc19857c482edb4fbc269430ef6c1bae69c4
Sachin Sen <sachinsen1920@gmail.com>
2025-07-01 22:23:22 +0530

    feat: implement admin middleware for JWT authentication

---
+++ Course Selling App/middleware/admin.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../config")
+
+
+function adminMiddleware(req, res, next){
+    const token = req.headers.authorization
+
+    if(!token){
+        res.json({
+            message: "Token is missing!"
+        })
+    }
+    const words = token.split(" ")
+    const jwtToken = words[1]
+    const decodeData = jwt.sign(jwtToken, JWT_SECRET)
+
+    if(decodeData.username){
+        next()
+    } else {
+        res.status(403).json({
+            message: "Invalid Credientials"
+        })
+    }
+}
+
+module.exports = adminMiddleware
