ed76324ee68dcfb4bcf1853c1725a2fff8afeebb
Sachin Sen <sachinsen1920@gmail.com>
2025-07-02 17:00:52 +0530

    feat: implement user middleware for JWT authentication

--- Course Selling App/middleware/user.js
+++ Course Selling App/middleware/user.js
@@ -1,1 +1,25 @@
 const jwt = require("jsonwebtoken")
+const { JWT_USER_SECRET } = require("../config")
+
+function userMiddleware(req, res, next){
+    const token = req.headers.authorization
+
+    if(!token){
+        res.json({
+            message: "Token is missing!"
+        })
+    }
+    const words = token.split(" ")
+    const jwtToken = words[1]
+    const decodeData = jwt.sign(jwtToken, JWT_USER_SECRET)
+
+    if(decodeData.username){
+        next()
+    } else {
+        res.status(403).json({
+            message: "Invalid Credientials"
+        })
+    }
+}
+
+module.exports = userMiddleware
