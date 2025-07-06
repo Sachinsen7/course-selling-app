e5f212877aee2a6489e09f6e81e07fd41f753e91
Sachin Sen <sachinsen1920@gmail.com>
2025-07-06 14:25:02 +0530

    implement api functionality

---
+++ Frontend/src/services/api.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { API_BASE_URL } from "../utils/constant";
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+
+    }
+
+    return config
+})
+
+export const getCourses = async (filters = {}) => {
+    try {
+        const response = await api.get('/courses', {params: filters})
+        return response.data
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "failed to fetch courses");
+        
+    }
+}
+
+export const getCourseById = async (id) => {
+    try {
+        const response = await api.get(`/courses/"${id}`)
+        return response.data
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "failed to fetch course");
+        
+    }
+
+}
+
+export const registerUser = async (userData) => {
+    try {
+        const response = await api.post("/auth/signup", userData)
+        return response.data
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "failed to register user");
+        
+    }
+}
