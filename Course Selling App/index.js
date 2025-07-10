e463ed6cd5240ebbbd692af2ccacaffe55ba6073
Sachin Sen <sachinsen1920@gmail.com>
2025-07-10 17:38:12 +0530

    add enrollment router update auth router

--- Course Selling App/index.js
+++ Course Selling App/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
-const { userRouter } = require("./routes/user")
-const { courseRouter } = require("./routes/course")
-const { adminRouter } = require("./routes/admin")
+const {authRouter} =  require("./routes/auth")
+const {enrollmentRouter} =  require("./routes/enrollments")
 app.use(express.json())
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 
 dotenv.config()
 
-app.use("/api/v1/user", userRouter)
-app.use("/api/v1/admin", adminRouter)
-app.use("/api/v1/course", courseRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/enrollment", enrollmentRouter)
+
 
 async function connectDB(){
     await mongoose.connect(process.env.MONGO_URI)
