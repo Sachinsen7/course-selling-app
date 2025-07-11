73db76c4df5b192a1af45241894835dab6866edd
Sachin Sen <sachinsen1920@gmail.com>
2025-07-11 15:04:21 +0530

    import routes

--- Course Selling App/index.js
+++ Course Selling App/index.js
@@ -3,6 +3,11 @@ const app = express();
 const {authRouter} =  require("./routes/auth")
 const {enrollmentRouter} =  require("./routes/enrollments")
 const {instructorRouter} =  require("./routes/instructor")
+const {searchRouter} =  require("./routes/search")
+const {paymentRouter} =  require("./routes/payments")
+const {reviewRouter} =  require("./routes/review")
+
+
 app.use(express.json())
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
@@ -12,6 +17,9 @@ dotenv.config()
 app.use("/api/auth", authRouter)
 app.use("/api/enrollment", enrollmentRouter)
 app.use("/api/instructor", instructorRouter)
+app.use("/api/search", searchRouter)
+app.use("/api/payment", paymentRouter)
+app.use("/api/review", reviewRouter)
 
 
 async function connectDB(){
