4631a9967626ecc0a2f814625941ff1778b7d179
Sachin Sen <sachinsen1920@gmail.com>
2025-07-11 14:41:58 +0530

    search api implement

---
+++ Course Selling App/routes/search.js
@@ -0,0 +1,21 @@
+const {Router} = require("express")
+const { CourseModel} = require("../db/db")
+const searchRouter = Router()
+
+searchRouter.get("/course", async (req, res) => {
+    const {q} = req.query
+
+    try {
+        const query = q ? {$or: [
+            {title: {$regex: q, $options: 'i'}},
+            {description: {$regex: q, $options: "i"}}
+        ]} : {}
+
+        const courses = await CourseModel.find(query)
+        res.json({message: "Courses retrived successfully", courses})
+    } catch (error) {
+        res.status(500).json({message: "Error Searching Courses", error: error.message})
+    }
+})
+
+module.exports = {searchRouter}
