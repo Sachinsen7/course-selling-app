3bec4d36b7da236886bc8a65a91f38405ee05f22
Sachin Sen <sachinsen1920@gmail.com>
2025-07-19 16:31:54 +0530

    add Quiz model with schema definition and validation

---
+++ Course Selling App/models/quiz.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const QuizSchema = new Schema({
+    lectureId: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Lecture',
+        required: true,
+        unique: true
+    },
+    courseId: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    questions: [{ 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Question'
+    }],
+    passPercentage: {
+        type: Number,
+        min: 0,
+        max: 100,
+        default: 70 
+    },
+    isPublished: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const QuizModel = mongoose.model("Quiz", QuizSchema);
+
+module.exports = QuizModel;
