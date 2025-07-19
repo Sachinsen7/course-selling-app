c00590e4614bef4d3f7945fd4b8340507e3b1513
Sachin Sen <sachinsen1920@gmail.com>
2025-07-19 14:52:47 +0530

    format code for consistency and readability in search route

--- Course Selling App/routes/search.js
+++ Course Selling App/routes/search.js
@@ -1,21 +1,27 @@
-const {Router} = require("express")
-const { CourseModel} = require("../db/db")
-const searchRouter = Router()
+const { Router } = require("express");
+const { CourseModel } = require("../db/db");
+const searchRouter = Router();
 
 searchRouter.get("/course", async (req, res) => {
-    const {q} = req.query
+  const { q } = req.query;
 
     try {
-        const query = q ? {$or: [
-            {title: {$regex: q, $options: 'i'}},
-            {description: {$regex: q, $options: "i"}}
-        ]} : {}
+    const query = q
+      ? {
+          $or: [
+            { title: { $regex: q, $options: "i" } },
+            { description: { $regex: q, $options: "i" } },
+          ],
+        }
+      : {};
 
-        const courses = await CourseModel.find(query)
-        res.json({message: "Courses retrived successfully", courses})
+    const courses = await CourseModel.find(query);
+    res.json({ message: "Courses retrived successfully", courses });
     } catch (error) {
-        res.status(500).json({message: "Error Searching Courses", error: error.message})
+    res
+      .status(500)
+      .json({ message: "Error Searching Courses", error: error.message });
     }
-})
+});
 
-module.exports = {searchRouter}
+module.exports = { searchRouter };
