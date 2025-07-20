94c1816c39a663134bf7e13fe5cc8f8848e62589
Sachin Sen <sachinsen1920@gmail.com>
2025-07-20 13:04:03 +0530

    fix spacing

--- Frontend/src/components/common/Loader.jsx
+++ Frontend/src/components/common/Loader.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import theme from "../../utils/theme"; 
+import theme from "../../utils/theme";
 
 function Loader() {
   return (
@@ -9,7 +9,7 @@ function Loader() {
       transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
     >
       <div
-        className="w-12 h-12 border-4 rounded-full" 
+        className="w-12 h-12 border-4 rounded-full"
         style={{ borderColor: `${theme.colors.primary.main} transparent transparent transparent` }}
       />
     </motion.div>
