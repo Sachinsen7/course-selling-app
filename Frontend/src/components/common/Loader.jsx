ea976259cc474ca1bf98abf9bb3c7cd65ea2e9aa
Sachin Sen <sachinsen1920@gmail.com>
2025-07-20 12:53:33 +0530

    fix loader styling

--- Frontend/src/components/common/Loader.jsx
+++ Frontend/src/components/common/Loader.jsx
@@ -1,21 +1,19 @@
-import { motion } from "framer-motion"
-import theme from "../../utils/theme"
+import { motion } from "framer-motion";
+import theme from "../../utils/theme"; 
 
 function Loader() {
-
-
   return (
-    <motion.div 
+    <motion.div
       className="flex justify-center items-center min-h-screen"
-      animate={{rotate: 360}}
-      transition={{duration: 1, repeat: Infinity, ease: "linear"}}
+      animate={{ rotate: 360 }}
+      transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
     >
       <div
-      className="w-8 h-8 border-4 rounded-full"
-      style={{borderColor: `${theme.colors.primary.main} traseparent`}}
+        className="w-12 h-12 border-4 rounded-full" 
+        style={{ borderColor: `${theme.colors.primary.main} transparent transparent transparent` }}
       />
     </motion.div>
-  )
+  );
 }
 
-export default Loader
+export default Loader;
