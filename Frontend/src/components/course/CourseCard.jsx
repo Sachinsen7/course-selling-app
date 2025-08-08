16d6782a855be3b712c71ab6851845eae70f3f6b
Sachin Sen <sachinsen1920@gmail.com>
2025-08-09 00:11:59 +0530

    Remove secondName references from CourseCard component and PropTypes

--- Frontend/src/components/course/CourseCard.jsx
+++ Frontend/src/components/course/CourseCard.jsx
@@ -108,7 +108,7 @@ function CourseCard({ course, className }) {
               <svg className="w-3 h-3 mr-1 text-[#4A8292]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
               </svg>
-              By: {course.creatorId.firstName} {course.creatorId.lastName || course.creatorId.secondName}
+              By: {course.creatorId.firstName} {course.creatorId.lastName}
             </p>
           )}
 
@@ -149,7 +149,6 @@ CourseCard.propTypes = {
     ]),
     creatorId: PropTypes.shape({
       firstName: PropTypes.string,
-      secondName: PropTypes.string,
       lastName: PropTypes.string,
     }),
   }).isRequired,
